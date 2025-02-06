8ec5baee4e9e98d062a1ed30a16731442de06361
Mekael Turner <mistnpeace@gmail.com>
2025-02-06 18:25:38 +0800

    [feat] added brute force solution

--- find-closest-number-to-zero/find-closest-number-to-zero.lisp
+++ find-closest-number-to-zero/find-closest-number-to-zero.lisp
@@ -1,2 +1,16 @@
 ;; LeetCode 2239
 ;; https://leetcode.com/problems/find-closest-number-to-zero/description/
+
+(defun find-closest-to-zero (nums)
+  "Returns the number in the list NUMS that is closest to zero.
+If two numbers are equally close, returns the larger one."
+  (when (null nums)
+    (error "Input list is empty."))
+  (let ((closest (first nums)))
+    (dolist (num (rest nums) closest)
+      (cond ((< (abs num) (abs closest))
+             (setf closest num))
+            ((= (abs num) (abs closest))
+             (when (> num closest)
+               (setf closest num)))))
+    closest))
